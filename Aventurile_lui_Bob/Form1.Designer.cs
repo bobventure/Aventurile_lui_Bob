d936814d272c4d38bca57490d7e3cd7a8458f6bb
bobventure <nantudorwhite@gmail.com>
2016-02-21 15:34:23 +0200

    Added project
    all

---
+++ Aventurile_lui_Bob/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Aventurile_lui_Bob
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.acceleratie = new System.Windows.Forms.Timer(this.components);
+            this.deacceleratie = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1867, 1338);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // acceleratie
+            // 
+            this.acceleratie.Interval = 1;
+            this.acceleratie.Tick += new System.EventHandler(this.acceleratie_Tick);
+            // 
+            // deacceleratie
+            // 
+            this.deacceleratie.Interval = 1;
+            this.deacceleratie.Tick += new System.EventHandler(this.deacceleratie_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1867, 1338);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer acceleratie;
+        private System.Windows.Forms.Timer deacceleratie;
+    }
+}
+
---
+++ Aventurile_lui_Bob/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Aventurile_lui_Bob
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap back;
+        Bitmap bobright;
+        Bitmap bobleft;
+        Bitmap bobimage;
+        Bitmap exit;
+        Point boblocation = Point.Empty;
+        Point exitlocation = Point.Empty;
+        int offSet = 0, vit;
+        int acc_secunde, acc_secunde_max = 15;
+        bool left = false, right = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        public void Game()
+        {
+            int imageNumber = panel1.Width / back.Width + 3;
+            using (Bitmap frame = new Bitmap(panel1.Width, panel1.Height))
+            {
+                using (Graphics graph = Graphics.FromImage(frame))
+                {
+                    for (int i = 0; i < imageNumber; i++)
+                    {
+                        graph.DrawImage(back, offSet % back.Width + i * back.Width - back.Width, 0);
+                    }
+                    graph.DrawImage(bobimage, boblocation);
+                    graph.DrawImage(exit,exitlocation);
+
+                }
+                using (Graphics graph = panel1.CreateGraphics())
+                {
+                    graph.DrawImage(frame, Point.Empty);
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.WindowState = FormWindowState.Maximized;
+            back = Properties.Resources.back;
+            bobleft = Properties.Resources.bobleft;
+            bobright = Properties.Resources.bobright;
+            bobimage = bobright;
+            exit = Properties.Resources.Exit;
+            timer1.Start();
+
+         }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            boblocation.X = panel1.Width / 2 - bobimage.Width / 2;
+            boblocation.Y = 210;
+            exitlocation.X = panel1.Width - exit.Width - 10;
+            exitlocation.Y = 5;
+            
+            if (left)
+            {
+                offSet = offSet + acc_secunde;
+                bobimage = bobleft;
+            }
+            if(right)
+            {
+                bobimage = bobright;
+                offSet = offSet - acc_secunde;
+            }
+            Game();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if(e.KeyCode == Keys.Left)
+            {
+                acceleratie.Start();
+                left = true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                acceleratie.Start();
+                right = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                acceleratie.Stop();
+                deacceleratie.Start();
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                
+                acceleratie.Stop();
+                deacceleratie.Start();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void deacceleratie_Tick(object sender, EventArgs e)
+        {
+            if(acc_secunde > 0)
+                acc_secunde = acc_secunde - acc_secunde_max / 3;
+            if (acc_secunde <= 0)
+            {
+                deacceleratie.Stop();
+                if (right == true)
+                    right = false;
+                if (left == true)
+                    left = false;
+                acc_secunde = 0;
+            }
+        }
+
+        private void acceleratie_Tick(object sender, EventArgs e)
+        { 
+            if(acc_secunde< acc_secunde_max)
+                acc_secunde++;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Point mousePt = new Point(e.X, e.Y);
+            if (mousePt.X > panel1.Width - exit.Width - 10 && mousePt.X < panel1.Width - 10 && mousePt.Y > panel1.Top + 10 && mousePt.Y < panel1.Top + 10 + exit.Height)
+                Application.Exit();
+        }
+    }
+}
